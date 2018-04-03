10ad982b7476d3b50af4de23b60b1c13f0ca9ff5
kaznak <kaznak.at.work@gmail.com>
2018-04-04 01:23:15 +0900

    feat(learnjs): add showView function.

---
+++ public/app.js
@@ -0,0 +1,9 @@
+
+'use strict'
+
+var learnjs = {};
+
+learnjs.showView = function(hash)	{
+    var problemView = $('<div class="problem-view">').text('Comming soon!');
+    $('.view-container').empty().append(problemView);
+}
